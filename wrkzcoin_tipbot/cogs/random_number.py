9f4e9a7afa3b925a179e3395c96c659a4663d9d2
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2021-12-02 21:25:33 +0700

    Update random_number.py

--- wrkzcoin_tipbot/cogs/random_number.py
+++ wrkzcoin_tipbot/cogs/random_number.py
@@ -3,9 +3,11 @@ import math, random
 
 import discord
 from discord.ext import commands
+from dislash import InteractionClient, Option, OptionType
 
 from config import config
 from Bot import *
+
 
 class RandomNumber(commands.Cog):
 
@@ -13,46 +15,63 @@ class RandomNumber(commands.Cog):
         self.bot = bot
 
 
+    async def rand_number(
+        self,
+        ctx,
+        number_string: str=None
+    ):
+        rand_numb = None
+        if number_string is None:
+            rand_numb = random.randint(1, 100)
+        else:
+            number_string = number_string.replace(",", "")
+            rand_min_max = number_string.split("-")
+            if len(rand_min_max) <= 1:
+                await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid range given. Example, use: `rand 1-50`')
+                return
+            try:
+                min_numb = int(rand_min_max[0])
+                max_numb = int(rand_min_max[1])
+                if max_numb - min_numb <= 0:
+                    await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid range given. Example, use: `rand 1-50`')
+                    return
+                else:
+                    rand_numb = random.randint(min_numb,max_numb)
+            except ValueError:
+                await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid range given. Example, use: `rand 1-50`')
+                return
+        if rand_numb:
+            try:
+                msg = await ctx.reply('{} Random number: **{:,}**'.format(ctx.author.mention, rand_numb))
+                await msg.add_reaction(EMOJI_OK_BOX)
+            except (discord.Forbidden, discord.errors.Forbidden, discord.errors.HTTPException) as e:
+                return
+
+
+    @inter_client.slash_command(usage="rand [1-100]",
+                                options=[
+                                    Option("range_number", "Enter a range from to (ex. 1-100)", OptionType.STRING, required=False)
+                                ],
+                                description="Generate a random number with TipBot.")
+    async def rand(
+        self, 
+        ctx, 
+        range_number: str=None
+    ):
+        await self.rand_number(ctx, range_number)
+
+
     @commands.command(
-        usage="random <1-100>", 
+        usage="random [1-100]", 
         aliases=['random'], 
-        description="Generate random number with TipBot."
+        description="Generate a random number with TipBot."
     )
     async def rand(
         self, 
         ctx, 
         randstring: str = None
     ):
-        rand_numb = None
-        if randstring is None:
-            rand_numb = random.randint(1,100)
-        else:
-            randstring = randstring.replace(",", "")
-            rand_min_max = randstring.split("-")
-            if len(rand_min_max) <= 1:
-                await ctx.message.add_reaction(EMOJI_ERROR)
-                await ctx.send(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid range given. Example, use: `rand 1-50`')
-                return
-            try:
-                min_numb = int(rand_min_max[0])
-                max_numb = int(rand_min_max[1])
-                if max_numb - min_numb <= 0:
-                    await ctx.message.add_reaction(EMOJI_ERROR)
-                    await ctx.send(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid range given. Example, use: `rand 1-50`')
-                    return
-                else:
-                    rand_numb = random.randint(min_numb,max_numb)
-            except ValueError:
-                await ctx.message.add_reaction(EMOJI_ERROR)
-                await ctx.send(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid range given. Example, use: `rand 1-50`')
-                return
-        if rand_numb:
-            await ctx.message.add_reaction(EMOJI_OK_BOX)
-            try:
-                msg = await ctx.send('{} Random number: **{:,}**'.format(ctx.author.mention, rand_numb))
-                await msg.add_reaction(EMOJI_OK_BOX)
-            except (discord.Forbidden, discord.errors.Forbidden, discord.errors.HTTPException) as e:
-                return
+        await self.rand_number(ctx, randstring)
 
 
 def setup(bot):
