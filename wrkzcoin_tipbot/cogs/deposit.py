8d6b84b683285337524f14f5e36ab2cb4f1f8500
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2021-12-02 20:38:51 +0700

    Minor update deposit

--- wrkzcoin_tipbot/cogs/deposit.py
+++ wrkzcoin_tipbot/cogs/deposit.py
@@ -149,7 +149,7 @@ class Deposit(commands.Cog):
     @inter_client.slash_command(usage="deposit <coin> [plain]",
                                 options=[
                                     Option('coin', 'Enter coin ticker/name', OptionType.STRING, required=True),
-                                    Option('option', 'plain/embed', OptionType.STRING, required=True, choices=[
+                                    Option('option', 'plain/embed', OptionType.STRING, required=False, choices=[
                                         OptionChoice("plain", "plain"),
                                         OptionChoice("embed", "embed")
                                     ]
@@ -158,41 +158,41 @@ class Deposit(commands.Cog):
                                 description="Get your tipjar's deposit address.")
     async def deposit(
         self, 
-        inter, 
+        ctx, 
         coin: str, 
         option: str="plain"
     ):
         prefix = "/"
         COIN_NAME = coin.upper()
         option = option.upper()
-        user_id = inter.author.id
+        user_id = ctx.author.id
         botLogChan = self.bot.get_channel(LOG_CHAN)
         # check if account locked
-        account_lock = await alert_if_userlock(inter, 'balance')
+        account_lock = await alert_if_userlock(ctx, 'balance')
         if account_lock:
-            await inter.reply(f'{EMOJI_RED_NO} {MSG_LOCKED_ACCOUNT}')
+            await ctx.reply(f'{EMOJI_RED_NO} {MSG_LOCKED_ACCOUNT}')
             return
         # end of check if account locked
 
         try:
-            wallet = await self.get_deposit_coin_user(inter.author.id, COIN_NAME)
+            wallet = await self.get_deposit_coin_user(ctx.author.id, COIN_NAME)
             if 'error' in wallet:
                 error_msg = wallet['error']
-                await inter.reply(f"{EMOJI_RED_NO} {error_msg}", ephemeral=True)
+                await ctx.reply(f"{EMOJI_RED_NO} {error_msg}", ephemeral=True)
                 return
             else:
                 deposit_address = wallet['balance_wallet_address']
                 # generate QR if not exists
                 gen_qr_address = await self.generate_qr_address(deposit_address)
                 if gen_qr_address is None:
-                    await inter.reply(f"'{EMOJI_RED_NO} Failed to generate QR.", ephemeral=True)
+                    await ctx.reply(f"'{EMOJI_RED_NO} Failed to generate QR.", ephemeral=True)
                     return
                 if option == "PLAIN":
-                    await inter.reply(f"Your **{COIN_NAME}**\'s deposit address: ```{deposit_address}```", ephemeral=True)
+                    await ctx.reply(f"Your **{COIN_NAME}**\'s deposit address: ```{deposit_address}```", ephemeral=True)
                 else:
                     # embed
-                    embed = discord.Embed(title=f'Your Deposit {inter.author.name}#{inter.author.discriminator} / **{COIN_NAME}**', description='{}'.format(get_notice_txt(COIN_NAME)), timestamp=datetime.utcnow(), colour=7047495)
-                    embed.set_author(name=inter.author.name, icon_url=inter.author.display_avatar)
+                    embed = discord.Embed(title=f'Your Deposit {ctx.author.name}#{ctx.author.discriminator} / **{COIN_NAME}**', description='{}'.format(get_notice_txt(COIN_NAME)), timestamp=datetime.utcnow(), colour=7047495)
+                    embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar)
                     embed.add_field(name="{} Deposit Address".format(COIN_NAME), value=f"`{deposit_address}`", inline=False)
                     if wallet['user_wallet_address']:
                         embed.add_field(name="Withdraw Address", value="`{}`".format(wallet['user_wallet_address']), inline=False)
@@ -208,12 +208,12 @@ class Deposit(commands.Cog):
                     prefix = "/"
                     embed.set_footer(text=f"Use:{prefix}deposit {COIN_NAME} plain (for plain text)")
                     try:
-                        return await inter.reply(embed=embed, ephemeral=True)
+                        return await ctx.reply(embed=embed, ephemeral=True)
                     except Exception as e:
                         traceback.print_exc(file=sys.stdout)
         except Exception as e:
             traceback.print_exc(file=sys.stdout)
-            await inter.reply(f"Internal error to get deposit for: {COIN_NAME}. Please report.", ephemeral=True)
+            await ctx.reply(f"Internal error to get deposit for: {COIN_NAME}. Please report.", ephemeral=True)
 
 
     @commands.command(
