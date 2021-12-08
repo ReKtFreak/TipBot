767dac7f55df37a233c014f9f8d6b676e9290bba
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2021-12-08 17:06:14 +0700

    Update tip_tipall.py

--- wrkzcoin_tipbot/cogs/tip_tipall.py
+++ wrkzcoin_tipbot/cogs/tip_tipall.py
@@ -2,9 +2,12 @@ import sys, traceback
 import time, timeago
 import discord
 from discord.ext import commands
+from dislash import InteractionClient, ActionRow, Button, ButtonStyle, Option, OptionType, OptionChoice
+import dislash
 
 from config import config
 from Bot import *
+
 
 class TipTipAll(commands.Cog):
 
@@ -18,61 +21,33 @@ class TipTipAll(commands.Cog):
             self.botLogChan = self.bot.get_channel(LOG_CHAN)
 
 
-    @commands.command(
-        usage="tipall <amount> <coin> [option]", 
-        description="Tip all users."
-    )
-    async def tipall(
-        self, 
-        ctx, 
-        amount: str, 
+    async def tip_all(
+        self,
+        ctx,
+        amount, 
         coin: str, 
-        option: str=None
+        option: str
     ):
         await self.bot_log()
         # check if bot is going to restart
-        if IS_RESTARTING:
-            await ctx.message.add_reaction(EMOJI_REFRESH)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Bot is going to restart soon. Wait until it is back for using this.')
-            return
+        if IS_RESTARTING: return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} Bot is going to restart soon. Wait until it is back for using this."}
         # check if account locked
         account_lock = await alert_if_userlock(ctx, 'tipall')
         if account_lock:
-            await ctx.message.add_reaction(EMOJI_LOCKED) 
-            await ctx.reply(f'{EMOJI_RED_NO} {MSG_LOCKED_ACCOUNT}')
-            return
+            return {"error": f"{EMOJI_RED_NO} {MSG_LOCKED_ACCOUNT}"}
         # end of check if account locked
-
-        # Check if tx in progress
-        if ctx.author.id in TX_IN_PROCESS:
-            await ctx.message.add_reaction(EMOJI_HOURGLASS_NOT_DONE)
-            msg = await ctx.reply(f'{EMOJI_ERROR} {ctx.author.mention} You have another tx in progress.')
-            await msg.add_reaction(EMOJI_OK_BOX)
-            return
-
-        amount = amount.replace(",", "")
-
-        try:
-            amount = Decimal(amount)
-        except ValueError:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid amount.')
-            return
-
-        if isinstance(ctx.channel, discord.DMChannel):
-            await ctx.reply(f'{EMOJI_RED_NO} This command can not be in private.')
-            return
-
-        COIN_NAME = coin.upper()
 
         # TRTL discord
         if ctx.guild.id == TRTL_DISCORD and COIN_NAME != "TRTL":
-            return
+            return {"error": f"{EMOJI_ERROR} {ctx.author.mention}, Not available for this coin in this guild."}
+            
+        # Check if tx in progress
+        if ctx.author.id in TX_IN_PROCESS:
+            return {"error": f"{EMOJI_ERROR} {ctx.author.mention} You have another tx in progress."}
 
+        COIN_NAME = coin.upper()
         if COIN_NAME not in ENABLE_COIN+ENABLE_COIN_DOGE+ENABLE_XMR+ENABLE_COIN_NANO+ENABLE_COIN_ERC+ENABLE_COIN_TRC+ENABLE_XCH:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} **INVALID TICKER**!')
-            return
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} **INVALID TICKER**!"}
 
         serverinfo = await store.sql_info_by_server(str(ctx.guild.id))
         if COIN_NAME in ENABLE_COIN_ERC:
@@ -86,50 +61,21 @@ class TipTipAll(commands.Cog):
         option_list = ["ALL", "ONLINE"]
         if option not in option_list:
             allow_option = ", ".join(option_list)
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            msg = await ctx.reply(f'{EMOJI_ERROR} {ctx.author.mention} TIPALL is currently support only option **{allow_option}**.')
-            await msg.add_reaction(EMOJI_OK_BOX)
-            return
-        print('TIPALL COIN_NAME:' + COIN_NAME)
+            return {"error": f"{EMOJI_ERROR} {ctx.author.mention} TIPALL is currently support only option **{allow_option}**."}
+
         if not is_coin_tipable(COIN_NAME):
-            msg = await ctx.reply(f'{EMOJI_ERROR} {ctx.author.mention} TIPPING is currently disable for {COIN_NAME}.')
-            await msg.add_reaction(EMOJI_OK_BOX)
-            return
+            return {"error": f"{EMOJI_ERROR} {ctx.author.mention} TIPPING is currently disable for {COIN_NAME}."}
+
         if is_maintenance_coin(COIN_NAME):
-            await ctx.message.add_reaction(EMOJI_MAINTENANCE)
-            await ctx.reply(f'{EMOJI_RED_NO} {COIN_NAME} in maintenance.')
-            return
+            return {"error": f"{EMOJI_RED_NO} {COIN_NAME} in maintenance."}
 
         # Check allowed coins
         tiponly_coins = serverinfo['tiponly'].split(",")
         if COIN_NAME == serverinfo['default_coin'].upper() or serverinfo['tiponly'].upper() == "ALLCOIN":
             pass
         elif COIN_NAME not in tiponly_coins:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} {COIN_NAME} not in allowed coins set by server manager.')
-            return
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} {COIN_NAME} not in allowed coins set by server manager."}
         # End of checking allowed coins
-
-        # Check flood of tip
-        floodTip = await store.sql_get_countLastTip(str(ctx.author.id), config.floodTipDuration)
-        if floodTip >= config.floodTip:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Cool down your tip or TX. or increase your amount next time.')
-            await self.botLogChan.send('A user reached max. TX threshold. Currently halted: `.tipall`')
-            return
-        # End of Check flood of tip
-
-        # Check if maintenance
-        if IS_MAINTENANCE == 1:
-            if int(ctx.author.id) in MAINTENANCE_OWNER:
-                pass
-            else:
-                await ctx.message.add_reaction(EMOJI_WARNING)
-                await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} {config.maintenance_msg}')
-                return
-        else:
-            pass
-        # End Check if maintenance
 
         notifyList = await store.sql_get_tipnotify()
         if coin_family == "ERC-20" or coin_family == "TRC-20":
@@ -147,15 +93,10 @@ class TipTipAll(commands.Cog):
             listMembers = [member for member in ctx.guild.members if member.status != discord.Status.offline and member.bot == False]
         elif option == "ALL":
             listMembers = [member for member in ctx.guild.members if member.bot == False]
-        print("Number of tip-all in {}: {}".format(ctx.guild.name, len(listMembers)))
+
         # Check number of receivers.
         if len(listMembers) > config.tipallMax_Offchain:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            try:
-                await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} The number of receivers are too many. This command isn\'t available here.')
-            except (discord.Forbidden, discord.errors.Forbidden, discord.errors.HTTPException) as e:
-                await ctx.author.send(f'{EMOJI_RED_NO} The number of receivers are too many in `{ctx.guild.name}`. This command isn\'t available here.')
-            return
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} The number of receivers are too many. This command isn\'t available here."}
         # End of checking receivers numbers.
         await logchanbot("{}#{} issuing TIPALL {}{} in {}/{} with {} users.".format(ctx.author.name, ctx.author.discriminator, 
                                                                                     num_format_coin(real_amount, COIN_NAME), COIN_NAME,
@@ -178,6 +119,9 @@ class TipTipAll(commands.Cog):
                     user_to = await store.sql_get_userwallet(str(member.id), COIN_NAME)
                 list_receivers.append(str(member.id))
 
+        if len(list_receivers) == 0:
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} There's not users to tip to."}
+
         user_from = await store.sql_get_userwallet(str(ctx.author.id), COIN_NAME)
         if user_from is None:
             if coin_family == "ERC-20":
@@ -189,75 +133,30 @@ class TipTipAll(commands.Cog):
             else:
                 user_from = await store.sql_register_user(str(ctx.author.id), COIN_NAME, SERVER_BOT, 0)
 
-        # get user balance
-        userdata_balance = await store.sql_user_balance(str(ctx.author.id), COIN_NAME)
-        xfer_in = 0
-        if COIN_NAME not in ENABLE_COIN_ERC+ENABLE_COIN_TRC: 
-            xfer_in = await store.sql_user_balance_get_xfer_in(str(ctx.author.id), COIN_NAME)
-        if COIN_NAME in ENABLE_COIN_DOGE+ENABLE_COIN_ERC+ENABLE_COIN_TRC:
-            actual_balance = float(xfer_in) + float(userdata_balance['Adjust'])
-        elif COIN_NAME in ENABLE_COIN_NANO:
-            actual_balance = int(xfer_in) + int(userdata_balance['Adjust'])
-            actual_balance = round(actual_balance / get_decimal(COIN_NAME), 6) * get_decimal(COIN_NAME)
-        else:
-            actual_balance = int(xfer_in) + int(userdata_balance['Adjust'])
-
-        # Negative check
-        try:
-            if actual_balance < 0:
-                msg_negative = 'Negative balance detected:\nUser: '+str(ctx.author.id)+'\nCoin: '+COIN_NAME+'\nAtomic Balance: '+str(actual_balance)
-                await logchanbot(msg_negative)
-        except Exception as e:
-            await logchanbot(traceback.format_exc())
-
+        balance_user = await get_balance_coin_user(str(ctx.author.id), COIN_NAME, discord_guild=False, server__bot=SERVER_BOT)
         if real_amount > MaxTx:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Transactions cannot be bigger than '
-                                    f'{num_format_coin(MaxTx, COIN_NAME)} '
-                                    f'{COIN_NAME}.')
-            return
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} Transactions cannot be bigger than {num_format_coin(MaxTx, COIN_NAME)} {COIN_NAME}."}
         elif real_amount < MinTx:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Transactions cannot be smaller than '
-                                    f'{num_format_coin(MinTx, COIN_NAME)} '
-                                    f'{COIN_NAME}.')
-            return
-        elif real_amount > actual_balance:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Insufficient balance to spread tip of '
-                                    f'{num_format_coin(real_amount, COIN_NAME)} '
-                                    f'{COIN_NAME}.')
-            return
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} Transactions cannot be smaller than {num_format_coin(MinTx, COIN_NAME)} {COIN_NAME}."}
+        elif real_amount > balance_user['actual_balance']:
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} Insufficient balance to spread tip of {num_format_coin(real_amount, COIN_NAME)} {COIN_NAME}."}
         elif (real_amount / len(list_receivers)) < MinTx:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Transactions cannot be smaller than '
-                                    f'{num_format_coin(MinTx, COIN_NAME)} '
-                                    f'{COIN_NAME} for each member. You need at least {num_format_coin(len(list_receivers) * MinTx, COIN_NAME)} {COIN_NAME}.')
-            return
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} Transactions cannot be smaller than {num_format_coin(MinTx, COIN_NAME)} {COIN_NAME} for each member. You need at least {num_format_coin(len(list_receivers) * MinTx, COIN_NAME)} {COIN_NAME}."}
 
         amountDiv = int(round(real_amount / len(list_receivers), 2))  # cut 2 decimal only
         if coin_family == "DOGE" or coin_family == "ERC-20" or coin_family == "TRC-20":
             amountDiv = round(real_amount / len(list_receivers), 4)
-            if (real_amount / len(list_receivers)) < MinTx:
-                await ctx.message.add_reaction(EMOJI_ERROR)
-                await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Transactions cannot be smaller than '
-                               f'{num_format_coin(MinTx, COIN_NAME)} '
-                               f'{COIN_NAME} for each member. You need at least {num_format_coin(len(list_receivers) * MinTx, COIN_NAME)} {COIN_NAME}.')
-                return
+            if real_amount / len(list_receivers) < MinTx:
+                return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} Transactions cannot be smaller than {num_format_coin(MinTx, COIN_NAME)} {COIN_NAME} for each member. You need at least {num_format_coin(len(list_receivers) * MinTx, COIN_NAME)} {COIN_NAME}."}
 
         if len(list_receivers) < 1:
-            await ctx.message.add_reaction(EMOJI_ERROR)
-            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} There is no one to tip to.')
-            return
+            return {"error": f"{EMOJI_RED_NO} {ctx.author.mention} There is no one to tip to."}
 
         # add queue also tipall
         if ctx.author.id not in TX_IN_PROCESS:
             TX_IN_PROCESS.append(ctx.author.id)
         else:
-            await ctx.message.add_reaction(EMOJI_HOURGLASS_NOT_DONE)
-            msg = await ctx.reply(f'{EMOJI_ERROR} {ctx.author.mention} You have another tx in progress.')
-            await msg.add_reaction(EMOJI_OK_BOX)
-            return
+            return {"error": f"{EMOJI_ERROR} {ctx.author.mention} You have another tx in progress."}
 
         tip = None
         try:
@@ -292,11 +191,11 @@ class TipTipAll(commands.Cog):
             tipAmount = num_format_coin(real_amount, COIN_NAME)
             ActualSpend_str = num_format_coin(amountDiv * len(list_receivers), COIN_NAME)
             amountDiv_str = num_format_coin(amountDiv, COIN_NAME)
-            await ctx.message.add_reaction(get_emoji(COIN_NAME))
             numMsg = 0
             total_found = 0
             max_mention = 40
             numb_mention = 0
+            tmp_message = await ctx.reply("Sending message...")
             if len(listMembers) < max_mention:
                 # DM all user
                 for member in listMembers:
@@ -374,6 +273,7 @@ class TipTipAll(commands.Cog):
                         except Exception as e:
                             await ctx.message.add_reaction(EMOJI_ZIPPED_MOUTH)
             # tipper shall always get DM. Ignore notifyList
+            await tmp_message.delete()
             try:
                 await ctx.author.send(
                     f'{EMOJI_ARROW_RIGHTHOOK} Tip of {tipAmount} '
@@ -384,6 +284,59 @@ class TipTipAll(commands.Cog):
             except (discord.Forbidden, discord.errors.Forbidden, discord.errors.HTTPException) as e:
                 await store.sql_toggle_tipnotify(str(ctx.author.id), "OFF")
             return
+
+
+    @dislash.guild_only()
+    @inter_client.slash_command(usage="tipall <amount> <coin> [online|all]",
+                                options=[
+                                    Option('amount', 'amount', OptionType.STRING, required=True),
+                                    Option('coin', 'coin', OptionType.STRING, required=True),
+                                    Option('option', 'online | all', OptionType.STRING, required=False)
+                                ],
+                                description="Tip all users in the guild.")
+    async def tipall(
+        self, 
+        ctx, 
+        amount: str, 
+        coin: str, 
+        option: str=None
+    ):
+        amount = amount.replace(",", "")
+        try:
+            amount = Decimal(amount)
+        except ValueError:
+            await ctx.message.add_reaction(EMOJI_ERROR)
+            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid amount.')
+            return
+        tip_all = await self.tip_all(ctx, amount, coin.upper(), option)
+        if tip_all and "error" in tip_all:
+            await ctx.reply(tip_all['error'], ephemeral=True)
+
+
+    @commands.guild_only()
+    @commands.command(
+        usage="tipall <amount> <coin> [option]", 
+        description="Tip all users in the guild."
+    )
+    async def tipall(
+        self, 
+        ctx, 
+        amount: str, 
+        coin: str, 
+        option: str=None
+    ):
+        amount = amount.replace(",", "")
+        try:
+            amount = Decimal(amount)
+        except ValueError:
+            await ctx.message.add_reaction(EMOJI_ERROR)
+            await ctx.reply(f'{EMOJI_RED_NO} {ctx.author.mention} Invalid amount.')
+            return
+        if option is None: option = "ONLINE"
+        tip_all = await self.tip_all(ctx, amount, coin.upper(), option)
+        if tip_all and "error" in tip_all:
+            msg = await ctx.reply(tip_all['error'], components=[row_close_message])
+            await store.add_discord_bot_message(str(msg.id), "DM" if isinstance(ctx.channel, discord.DMChannel) else str(ctx.guild.id), str(ctx.author.id))
 
 
 def setup(bot):
